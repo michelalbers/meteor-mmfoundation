9375a44bb3a99a9e8f1e60a79fa15d71e5110275
Michel Albers <m.albers@interwebs-ug.de>
2015-11-03 11:07:05 +0100

    Initial

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'interwebs:mm-foundation',
+  version: '1.0.0',
+  summary: 'Wrapper for angular mm-foundation (http://pineconellc.github.io/angular-foundation/)',
+  git: ''
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+  api.use('urigo:angular');
+  api.addFiles('mm-foundation-tpls-0.6.0.js', ['client']);
+});
